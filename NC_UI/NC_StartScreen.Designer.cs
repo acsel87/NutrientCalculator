af34ab886268b706b6514683cf10204e0ed99e33
Acsel <alex87_andru@yahoo.com>
2018-06-30 00:01:08 +0300

    Added basic UI structure

--- NC_UI/NC_StartScreen.Designer.cs
+++ NC_UI/NC_StartScreen.Designer.cs
@@ -29,17 +29,194 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NC_StartScreen));
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.homeTab = new System.Windows.Forms.TabPage();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.exportButton = new System.Windows.Forms.Button();
+            this.foodTab = new System.Windows.Forms.TabPage();
+            this.recipesTab = new System.Windows.Forms.TabPage();
+            this.dayPlansTab = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.tabControl.SuspendLayout();
+            this.homeTab.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tabControl
+            // 
+            resources.ApplyResources(this.tabControl, "tabControl");
+            this.tabControl.Controls.Add(this.homeTab);
+            this.tabControl.Controls.Add(this.foodTab);
+            this.tabControl.Controls.Add(this.recipesTab);
+            this.tabControl.Controls.Add(this.dayPlansTab);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.tabControl.Tag = "";
+            // 
+            // homeTab
+            // 
+            resources.ApplyResources(this.homeTab, "homeTab");
+            this.homeTab.Controls.Add(this.tableLayoutPanel1);
+            this.homeTab.Name = "homeTab";
+            this.homeTab.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.exportButton, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 4, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label6, 5, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label7, 6, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label8, 7, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label9, 8, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label10, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label11, 0, 47);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label12, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label13, 0, 21);
+            this.tableLayoutPanel1.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.FixedSize;
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            // 
+            // exportButton
+            // 
+            resources.ApplyResources(this.exportButton, "exportButton");
+            this.exportButton.Name = "exportButton";
+            this.exportButton.UseVisualStyleBackColor = true;
+            // 
+            // foodTab
+            // 
+            resources.ApplyResources(this.foodTab, "foodTab");
+            this.foodTab.BackColor = System.Drawing.Color.White;
+            this.foodTab.Name = "foodTab";
+            // 
+            // recipesTab
+            // 
+            resources.ApplyResources(this.recipesTab, "recipesTab");
+            this.recipesTab.Name = "recipesTab";
+            this.recipesTab.UseVisualStyleBackColor = true;
+            // 
+            // dayPlansTab
+            // 
+            resources.ApplyResources(this.dayPlansTab, "dayPlansTab");
+            this.dayPlansTab.Name = "dayPlansTab";
+            this.dayPlansTab.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.Name = "label9";
+            // 
+            // label10
+            // 
+            resources.ApplyResources(this.label10, "label10");
+            this.label10.Name = "label10";
+            // 
+            // label11
+            // 
+            resources.ApplyResources(this.label11, "label11");
+            this.label11.Name = "label11";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label12
+            // 
+            resources.ApplyResources(this.label12, "label12");
+            this.label12.Name = "label12";
+            // 
+            // label13
+            // 
+            resources.ApplyResources(this.label13, "label13");
+            this.label13.Name = "label13";
             // 
             // NC_StartScreen
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tabControl);
             this.Name = "NC_StartScreen";
+            this.tabControl.ResumeLayout(false);
+            this.homeTab.ResumeLayout(false);
+            this.homeTab.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage foodTab;
+        private System.Windows.Forms.TabPage recipesTab;
+        private System.Windows.Forms.TabPage homeTab;
+        private System.Windows.Forms.Button exportButton;
+        private System.Windows.Forms.TabPage dayPlansTab;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
     }
 }
